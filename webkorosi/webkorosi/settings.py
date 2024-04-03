e6ff9955bcaf231f598b3cb7f56e696f23696978
Nibras Ardyansyah <nibrasbahy@gmail.com>
2024-04-03 16:22:20 +0700

    Update settings.py

--- webkorosi/webkorosi/settings.py
+++ webkorosi/webkorosi/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ['.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
-    "myapp.apps.MyappConfig",
+    # "myapp.apps.MyappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles"
+    "django.contrib.staticfiles",
+    "myapp"
 ]
 
 MIDDLEWARE = [
